48f09bddadbfb13fe9f1aee597ac4d9dfa4f5d4e
ramezrb <ramez.rb@gmail.com>
2020-02-28 23:59:15 +0200

    update group section

---
+++ HMS.WebApp/src/app/Reception/groups/add-company-modal/add-company-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-company-modal',
+  templateUrl: './add-company-modal.component.html',
+  styleUrls: ['./add-company-modal.component.css']
+})
+export class AddCompanyModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
